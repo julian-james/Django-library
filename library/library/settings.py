ca4089a4f5afc979af80b2c2def68740af2fabdc
Julian James <julian@Julians-MacBook-Pro.local>
2022-03-31 21:56:33 +0100

    set STATIC_ROOT in settings

--- library/library/settings.py
+++ library/library/settings.py
@@ -131,6 +131,8 @@ USE_TZ = True
 
 STATIC_URL = '/library/loan/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
