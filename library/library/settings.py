b2e074b9c382058b80ed21167aba24bc24ed41a9
Julian James <julian@Julians-MacBook-Pro.local>
2022-03-31 22:53:13 +0100

    removed STATICFILES_DIRS

--- library/library/settings.py
+++ library/library/settings.py
@@ -144,9 +144,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 # STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
