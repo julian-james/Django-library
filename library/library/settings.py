bb50624f683af1d6e23fd6a562f681c6a30e67e8
Julian James <julian@Julians-MacBook-Pro.local>
2022-03-31 22:47:46 +0100

    changed staticfiles_storage

--- library/library/settings.py
+++ library/library/settings.py
@@ -147,8 +147,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 # Default primary key field type
