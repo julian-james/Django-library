208bd753654ad2e518189d11742173b1fc0813eb
Julian James <julian@Julians-MacBook-Pro.local>
2022-03-31 22:49:47 +0100

    changed static_url

--- library/library/settings.py
+++ library/library/settings.py
@@ -138,7 +138,8 @@ USE_TZ = True
 
 STATIC_HOST = os.environ.get("DJANGO_STATIC_HOST", "")
 
-STATIC_URL = '/library/loan/static/'
+# STATIC_URL = '/library/loan/static/'
+STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_ROOT = BASE_DIR / "staticfiles"
